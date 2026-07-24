b9d3003a576b455f4fd231432598b717079de8ac
Danilo <danilojmini2@gmail.com>
2026-07-24 16:12:53 -0300

    Atualizando e finalizando aula de Vite

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,11 +3,13 @@ import styles from './Card.module.css';
 export default function Card({ imagem, nome, descricao, campeao }) {
     return (
         <article className={styles.card}>
-            <img src={imagem} alt={nome} />
-            <h2>{nome}</h2>
-            <p className={styles.descricao}>{descricao}</p>
+            <img src={imagem || '/img/sem_foto.png'} alt={nome} />
+            <h2>{nome || 'Sem nome'}</h2>
+            <p className={styles.descricao}>{descricao || 'Sem descrição'}</p>
             <p>
-                <span>Campeão:</span> {'🏆'.repeat(campeao)}
+                <p>
+                    <span>Campeão:</span> {campeao > 0 ? '🏆'.repeat(campeao) : 'Nunca campeão'}
+                </p>
             </p>
         </article>
     );
